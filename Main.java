de7064a83e6fde28f098db0960623ddcb1263188
Serenella9220 <118180225+Serenella9220@users.noreply.github.com>
2024-03-06 11:31:34 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(5, 10);
+        System.out.println("Area of rectangle: " + rectangle.calculateArea());
+
+        Triangle triangle = new Triangle(4, 6);
+        System.out.println("Area of triangle: " + triangle.calculateArea());
+    }
+   
+    
+    
+    
+}
---
+++ Triangle.java
@@ -0,0 +1,15 @@
+
+class Triangle implements Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return (base * height) / 2;
+    }
+}
